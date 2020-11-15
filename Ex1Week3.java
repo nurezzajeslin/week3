3ee4b5e1a73102110cd6dc5e7524d8d72233f666
nurezzajeslin <72841306+nurezzajeslin@users.noreply.github.com>
2020-11-15 16:46:41 +0800

    Add files via upload

---
+++ Ex1Week3.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+public class Ex1Week3{
+	public static void main(String[] nilaiTerima){
+		
+		int noInt=10, x;
+
+		//if(noInt==10)
+		//	System.out.println("sama");
+		//else
+			//System.out.println("tak sama");
+
+		System.out.println(noInt==10? "sama" : "tak sama ");
+
+		if(noInt==10)
+			x=3;
+		else
+			x=19;
+
+		x=noInt==10? 3 : 19;
+		//ternary operator
+		System.out.println("x is " + x);
+
+
+
+
+
+
+
+
+
+		//String nama;
+		//int nombor;
+		//double noDouble;
+
+		//Scanner input =new Scanner(System.in);
+
+
+		//System.out.println("Enter a number : ");
+		//nombor = input.nextInt();
+		//System.out.println("Number is " + nombor);
+
+		//input.nextLine();
+
+		//System.out.println("Enter a name : ");
+		//nama = input.nextLine();
+		//System.out.println("Name is " + nama);
+
+
+		//System.out.println("Enter a double : ");
+		//noDouble = input.nextDouble();
+		//System.out.println("Number is " + noDouble);
+
+		
+
+
+		//input.close();
+
+
+		//int noInt='10';
+		//final static char CHARSAYA=53; //asci code
+		//System.out.println("Char "+ CHARSAYA);
+		//CHARSAYA=100;
+		//System.out.println("Char "+ CHARSAYA);
+
+
+		//int[] tataInt ={10,20,30,40};
+
+		//for(int i=0; i<tataInt.length; i++)
+		//{
+		//	System.out.println(tataInt[i]);
+		//}
+
+		//for(  int x : tataInt)
+		//{
+		//	System.out.println("for each : "+ x);
+		//}
+
+
+
+
+
+		//int noInt=123;
+		//int noInt2=345;
+		//double noDouble=123.456;
+
+		//System.out.format("%6d %4d", noInt, noInt2);
+		//System.out.format("%4d", noInt2);
+		//System.out.format("%7.2F", noDouble);
+		//System.out.format("\nRM%7.3F dan %6d", noDouble, no Int);
+
+
+
+
+
+
+		//System.out.println("Nilai 1: " + nilaiTerima[0]);
+		//System.out.println("Nilai 1: " + nilaiTerima[1]);
+		//System.out.println("Nilai 1: " + nilaiTerima[2]);
+		
+		//int sum=0;
+
+		//for(int i=0; i<nilaiTerima.length; i++){
+			//System.out.println("Nilai 1: " + nilaiTerima[i]);
+			//sum+= Integer.parseInt(nilaiTerima[i]);
+			//System.out.println("Nilai sum : "+sum);
+			
+		//}
+	}
+	
+}
